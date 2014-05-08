f341ac01c8aab18d052caf3bc9605c1df504034c
Phillip Alexander <git@phillipalexander.io>
2014-05-08 12:57:54 -0700

    Add command to create repos

---
+++ cmds/repo.js
@@ -0,0 +1,65 @@
+/* repo commander component
+ * To use add require('../cmds/repo.js')(program) to your commander.js based node executable before program.parse
+ */
+'use strict';
+
+var request = require("superagent");
+var fs = require('fs');
+var path = require('path');
+
+var config;
+
+if (fs.existsSync(path.join(__dirname, '../config.json'))) {
+  config = require('../config.json');
+} else {
+  console.log('You must set your GitHub token')
+  console.log('Run the following command to create a config.json file, add your token, then try again.')
+  console.log('cp ' + path.join(__dirname, '../config.json.example') + ' ' + path.join(__dirname, '../config.json') + ' && vim ' + path.join(__dirname, '../config.json'))
+  process.exit();
+}
+
+module.exports = function(program) {
+
+  program
+    .command("rc <fullname>")
+    .version("0.0.1")
+    .description("Create repo on GitHub")
+    .action(function(fullname) {
+      var owner = fullname.split("/")[0];
+      var repo = fullname.split("/")[1];
+
+      // first try as org
+      request
+        .post("https://api.github.com/orgs/" + owner + "/repos")
+        .query({access_token: config.github.token})
+        .set('Content-Type', 'application/json')
+        .send({name: repo})
+        .send({private: true})
+        .send({has_issues: false})
+        .send({has_wiki: false})
+        .send({auto_init: false})
+        .end(function(res) {
+          if(res.ok) {
+            console.log("url: " + res.body.html_url);
+          } else if (res.notFound) {
+            // if creating as an org fails, try as user
+            request
+              .post("https://api.github.com/user/repos")
+              .query({access_token: config.github.token})
+              .set('Content-Type', 'application/json')
+              .send({name: repo})
+              .send({private: true})
+              .send({has_issues: false})
+              .send({has_wiki: false})
+              .send({auto_init: false})
+              .end(function(res) {
+                if(res.ok) {
+                  console.log("url: " + res.body.html_url);
+                }
+              });
+          }
+        });
+
+    });
+
+};
